24a72f8f0280ded4ca326de7990ee84587774462
Tracy <duyuetong@ruc.edu.cn>
2023-12-30 20:58:25 +0800

    one plot added for barchart
    在dashboard里面加了一个柱状图

---
+++ src_in_progress_by_frank/patent/forms.py
@@ -0,0 +1,161 @@
+from flask import flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextAreaField, FloatField, IntegerField
+from wtforms.validators import DataRequired, EqualTo, ValidationError, Length, Email, InputRequired
+from patent.models import Applicant, Visitor, Inspector
+from flask_login import current_user
+
+
+class RegistrationForm(FlaskForm):
+    role=SelectField("Select role", coerce=str, choices=[("1","Applicant"), ("2","Visitor"),("3","Inspector")])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_email(self, email):
+        if self.role.data == "1":
+            table = Applicant
+        elif self.role.data == "2":
+            table = Visitor
+        elif self.role.data =="3":
+            table = Inspector
+        if table.query.filter_by(email=email.data).first():
+            raise ValidationError("Duplicate email")
+
+
+class LoginForm(FlaskForm):
+    role = SelectField("Select role",coerce=str,choices=[("1","Applicant"),("2","Visitor")])
+    email = StringField('Mail',validators=[DataRequired(), Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    remember = BooleanField("Remember me")
+    submit = SubmitField('Sign in')
+
+
+class UpdateInfo(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Mail',validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_email(self, email):
+        if current_user.table_name == "Applicant":
+            table = Applicant
+        elif current_user.table_name == "Visitor":
+            table = Visitor
+        user = table.query.filter_by(email=email.data).first()
+        if user and user.username !=current_user.username:
+            raise ValidationError("Duplicate email")
+
+
+class UpdatePasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired(),Length(min=6, max=20)])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Update')
+
+
+class UpdateApplicantForm(FlaskForm):
+    affliated_organization = StringField("affliated_organization",validators=[InputRequired(),Length(min=2,max=20)])
+    address = StringField("Address",validators=[InputRequired(),Length(min=10,max=40)])
+    telephone = StringField("Telephone",validators=[InputRequired(),Length(max=20,min=9)])
+    submit = SubmitField("Update")
+
+
+class UpdateVisitorForm(FlaskForm):
+    # address = StringField('address',validators=[InputRequired(),Length(min=5, max=40)])
+    telephone = StringField("Telephone", validators=[InputRequired(), Length(max=20, min=9)])
+    submit = SubmitField('Update')
+
+
+
+class ProductForm(FlaskForm):
+    name = StringField('Product name',validators=[DataRequired(), Length(min=2, max=40)])
+    price =FloatField("Product price", validators=[DataRequired()])
+    count = IntegerField("Product count", validators=[DataRequired()])
+    confirm = IntegerField("Confirm Product count",validators=[DataRequired(), EqualTo("count")])
+    submit = SubmitField("Add")
+
+
+class UpdateProductForm(FlaskForm):
+    name = StringField('Product name',validators=[InputRequired(), Length(min=2, max=40)])
+    price = FloatField("Product price", validators=[InputRequired()])
+    count = IntegerField("Product count", validators=[InputRequired()])
+    confirm = IntegerField("Confirm Product count", validators=[InputRequired(), EqualTo("count")])
+    submit = SubmitField("Update")
+
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, DateTimeField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Length, Optional
+
+class GApplicationInProgress(FlaskForm):
+
+    d_ipc = SelectField(
+        'D-IPC',
+        choices = [(0,0),(1,1)],
+        validators=[DataRequired()]
+    )
+    ipc_section = StringField(
+        'IPC Section (Maximum Length = 32)', 
+        validators=[DataRequired(), Length(max=32)]
+    )
+    patent_type = SelectField(
+        'Patent Type', 
+        choices=[('utility', 'Utility'), ('design', 'Design'), ('plant', 'Plant'), ('reissue', 'Reissue')],
+        validators=[DataRequired()]
+    )
+    patent_title = TextAreaField(
+        'Patent Title', 
+        validators=[DataRequired()]
+    )
+    patent_abstract = TextAreaField(
+        'Patent Abstract', 
+        validators=[DataRequired()]
+    )
+    wipo_kind = StringField(
+        'WIPO Kind (Maximum Length = 3)', 
+        validators=[DataRequired(), Length(max=3)]
+    )
+
+    def add_inventor_fields(self, number_of_inventors):
+        for i in range(1, number_of_inventors + 1):
+            setattr(self, f'inventor_name{i}', StringField(f'Inventor Name {i}'))
+            setattr(self, f'male_flag{i}', SelectField(f'Inventor Gender {i}', choices=[('1', 'Male'), ('0', 'Female')]))
+
+    submit = SubmitField("Submit")
+
+class GPatentSearch(FlaskForm):
+
+    d_ipc = SelectField(
+        'D-IPC',
+        choices = [(0,0),(1,1),('NA','Both')],
+        validators=[]
+    )
+    ipc_section = StringField(
+        'IPC Section (Maximum Length = 32)', 
+        validators=[Length(max=32)]
+    )
+    patent_type = SelectField(
+        'Patent Type', 
+        choices=[('utility', 'utility'), ('design', 'design'), ('plant', 'plant'), ('reissue', 'reissue'),('NA','Any will be fine!')],
+        validators=[]
+    )
+    patent_keyword = TextAreaField(
+        'Patent Keyword (Required)', 
+        validators=[DataRequired()]
+    )
+    patent_abstract_keyword = TextAreaField(
+        'Patent Abstract', 
+        validators=[]
+    )
+    wipo_kind = StringField(
+        'WIPO Kind (Maximum Length = 3)', 
+        validators=[Length(max=3)]
+    )
+    inventor = StringField(
+        'Inventor Name', 
+        validators=[Length(max=32)]
+    )
+    
+
+    submit = SubmitField("Search")
--- src_in_progress_by_frank/patent/routes.py
+++ src_in_progress_by_frank/patent/routes.py
@@ -6,12 +6,25 @@ from flask_login import current_user, logout_user, login_user, login_required
 from datetime import datetime
 from sqlalchemy import or_, and_
 from flask import session
+from sqlalchemy import func
 
 @app.route('/')
 @app.route("/home") # home page
 def home():
     first_ten_patents = GPatent.query.limit(10).all()
     return render_template("home.html", patents=first_ten_patents, title = "Home")
+
+@app.route("/dashboard")
+def  dashboard():
+    results = (db.session.query(GLocation.country, func.count(GLocation.patent_number).label('number'))
+               .group_by(GLocation.country)
+               .order_by(func.count(GLocation.patent_number).desc())
+               .limit(8)
+               .all())
+    return render_template("dashboard.html",result_left1= results,title="Dashboard")
+
+
+
 
 @app.route("/register",methods=["GET", 'POST']) # register
 def register():
@@ -27,7 +40,7 @@ def register():
             table = Visitor
         elif form.role.data == "3":
             role = Inspector()
-            table_name = "Inspector"
+            table_name = "Visitor"
             table = Inspector
         hashed_password = bcrypt.generate_password_hash(password=form.password.data).decode("utf-8")
         role.username = form.username.data
@@ -116,7 +129,7 @@ def inspector_account(username):
     if insepctor1.telephone == "null": # if any of the info is not submitted
         flash("Please complete your personal information as soon as possible","warning")
         return redirect(url_for("inspector_detail_manage"))
-    return render_template("inspector_account.html", username=username)
+    return render_template("visitor_account.html", username=username)
 
 @app.route("/update/info",methods=["GET","POST"])
 @login_required
@@ -208,7 +221,7 @@ def visitor_detail_manage(): # supplier_shipper_manage
 def inspector_detail_manage():
     if current_user.table_name != "Inspector":
         abort(403)
-    form = UpdateInspectorForm()
+    form = UpdateVisitorForm()
     inspector1 = Inspector.query.filter_by(id=current_user.table_id).first() # look for visitor info
     if form.validate_on_submit():
         inspector1.telephone = form.telephone.data
@@ -217,7 +230,7 @@ def inspector_detail_manage():
         flash("Profile updated successfully","success")
     elif request.method == "GET":
         form.telephone.data = inspector1.telephone
-    return render_template("update_inspector.html", form=form)
+    return render_template("update_visitor.html", form=form)
 
 @app.route("/applicant/apply",methods=["GET","POST"])
 @login_required
@@ -363,12 +376,5 @@ def cite(id):
         flash("Claimed Successfully!", "success")
     else:
         flash("Patent not found.", "danger")
-
-    return redirect(url_for("home"))
-
-@app.route("/cite/<id>")
-@login_required
-def inspector_process_applications(id):
-    
 
     return redirect(url_for("home"))
